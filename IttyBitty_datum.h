9cd0005d7433962d8826e1158dbf703bd894efa4
FancyFoxGems <tjbtech@yahoo.com>
2016-11-12 17:23:24 -0700

    asdf

--- IttyBitty_datum.h
+++ IttyBitty_datum.h
@@ -118,13 +118,13 @@ namespace IttyBitty
 #pragma region HELPER GLOBAL FUNCTION DEFINITIONS
 
 	template DEFAULT_T_CLAUSE
-	STATIC PCHAR StringInsertValue(CONST T & value, PCHAR buffer, CBYTE radix = 0x10)
+	STATIC PCHAR StringInsertValue(CONST T & value, PCHAR buffer)
 	{
 		CHAR valStr[2 * T_SIZE + 1];
 
 		valStr[2 * T_SIZE] = '\0';
 
-		itoa(value, valStr, radix);
+		itoa(value, valStr, 0x10);
 
 		BYTE lenDiff = 2 * T_SIZE - strlen(valStr);
 		for (SIZE i = 0; i < lenDiff; i++)
@@ -139,7 +139,7 @@ namespace IttyBitty
 	}
 
 	template DEFAULT_T_CLAUSE
-	STATIC PCCHAR StringReadValue(T & value, PCCHAR data, CBYTE radix = 0x10)
+	STATIC PCCHAR StringReadValue(T & value, PCCHAR data)
 	{
 		CHAR valStr[2 * T_SIZE + 1];
 
