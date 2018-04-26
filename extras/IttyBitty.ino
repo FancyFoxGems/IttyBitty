e28b4f0fdd0f495f402ec51bc483424dd8caa632
FancyFoxGems <tjbtech@yahoo.com>
2018-04-26 12:01:39 -0700

    asdf

---
+++ extras/IttyBitty.ino
@@ -0,0 +1,9 @@
+/**********************************************************************************************
+* This file is part of the Itty Bitty Arduino library.                                        *
+* Copyright © 2016 Thomas J. Biuso III  ALL RIGHTS RESERVED...WHATEVER THAT MEANS.            *
+* RELEASED UNDER THE GPL v3.0 LICENSE; SEE <LICENSE> FILE WITHIN DISTRIBUTION ROOT FOR TERMS. *
+***********************************************************************************************/
+
+void setup() { }
+
+void loop() { }
